d6f1db3204e02459a42fcedc1dc5211512e4e547
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-04-11 12:11:14 +0300

    build boot image now uses a container to mask dependencies

--- pkg/compilers/generic.go
+++ pkg/compilers/generic.go
@@ -1,23 +1,150 @@
 package compilers
 
-import unikos "github.com/emc-advanced-dev/unik/pkg/os"
-import "io/ioutil"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
+	"github.com/docker/engine-api/types/container"
+	"github.com/docker/engine-api/types/network"
+	"github.com/docker/engine-api/types/strslice"
+	unikos "github.com/emc-advanced-dev/unik/pkg/os"
+	"golang.org/x/net/context"
+)
 
 func BuildBootableImage(kernel, cmdline string) (string, error) {
-	// run container that builds the boot image
-	//TODO: create a container  from this function
-	rootFile, err := ioutil.TempFile("", "")
+	directory, err := ioutil.TempDir("", "")
 	if err != nil {
 		return "", err
 	}
-	rootFileName := rootFile.Name()
-	rootFile.Close()
+	defer os.RemoveAll(directory)
+	kernelBaseName := "program.bin"
 
-	size := unikos.MegaBytes(100)
-
-	if err := unikos.CreateBootImageWithSize(rootFileName, size, kernel, cmdline); err != nil {
+	if err := unikos.CopyFile(kernel, path.Join(directory, kernelBaseName)); err != nil {
 		return "", err
 	}
 
-	return rootFileName, nil
+	cmds := []string{"-d", "/opt/vol", "-p", kernelBaseName, "-a", cmdline}
+	binds := []string{directory + ":/opt/vol/"}
+
+	if err := RunContainer("boot-creator", cmds, binds, true); err != nil {
+		return "", err
+	}
+
+	resultFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		return "", err
+	}
+	resultFile.Close()
+
+	if err := os.Rename(path.Join(directory, "vol.img"), resultFile.Name()); err != nil {
+		return "", err
+	}
+
+	return resultFile.Name(), nil
+}
+
+func RunContainer(imageName string, cmds, binds []string, privileged bool) error {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	config := &container.Config{
+		Image: imageName,
+		Cmd:   strslice.StrSlice(cmds),
+	}
+	hostConfig := &container.HostConfig{
+		Binds:      binds,
+		Privileged: privileged,
+	}
+	networkingConfig := &network.NetworkingConfig{}
+	containerName := ""
+
+	container, err := cli.ContainerCreate(context.Background(), config, hostConfig, networkingConfig, containerName)
+	if err != nil {
+		log.WithField("err", err).Error("Error creating container")
+		return err
+	}
+	defer cli.ContainerRemove(context.Background(), types.ContainerRemoveOptions{ContainerID: container.ID})
+
+	log.WithField("id", container.ID).Error("Created container")
+
+	if err := cli.ContainerStart(context.Background(), container.ID); err != nil {
+		log.WithField("err", err).Error("ContainerStart")
+		return err
+	}
+
+	status, err := cli.ContainerWait(context.Background(), container.ID)
+	if err != nil {
+		return err
+	}
+
+	if status != 0 {
+		log.WithField("status", status).Error("Container exit status non zero")
+
+		options := types.ContainerLogsOptions{
+			ContainerID: container.ID,
+			ShowStdout:  true,
+			ShowStderr:  true,
+			Follow:      true,
+			Tail:        "all",
+		}
+		reader, err := cli.ContainerLogs(context.Background(), options)
+		if err != nil {
+			log.WithField("err", err).Error("ContainerLogs")
+			return err
+		}
+		defer reader.Close()
+
+		if res, err := ioutil.ReadAll(reader); err == nil {
+			log.Error(string(res))
+		} else {
+			log.WithField("err", err).Warn("failed to get logs")
+		}
+
+		return errors.New("Returned non zero status")
+	}
+
+	return nil
+}
+
+// rump special json
+func ToRumpJson(c RumpConfig) (string, error) {
+
+	blk := c.Blk
+	c.Blk = nil
+
+	jsonConfig, err := json.Marshal(c)
+	if err != nil {
+		return "", err
+	}
+
+	blks := ""
+	for _, b := range blk {
+
+		blkjson, err := json.Marshal(b)
+		if err != nil {
+			return "", err
+		}
+		blks += fmt.Sprintf("\"blk\": %s,", string(blkjson))
+	}
+	var jsonString string
+	if len(blks) > 0 {
+
+		jsonString = string(jsonConfig[:len(jsonConfig)-1]) + "," + blks[:len(blks)-1] + "}"
+
+	} else {
+		jsonString = string(jsonConfig)
+	}
+
+	return jsonString, nil
+
 }
--- pkg/compilers/rump.go
+++ pkg/compilers/rump.go
@@ -2,19 +2,11 @@ package compilers
 
 import (
 	"archive/tar"
-	"encoding/json"
 	"io"
 
-	"github.com/docker/engine-api/client"
-	"github.com/docker/engine-api/types"
-	"github.com/docker/engine-api/types/container"
 	uniktypes "github.com/emc-advanced-dev/unik/pkg/types"
 
 	log "github.com/Sirupsen/logrus"
-
-	"github.com/docker/engine-api/types/network"
-
-	"golang.org/x/net/context"
 
 	"errors"
 	"fmt"
@@ -103,99 +95,5 @@ func (r *RunmpCompiler) extractTar(tarArchive io.ReadCloser, localFolder string)
 
 func (r *RunmpCompiler) runContainer(localFolder string) error {
 
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		return err
-	}
-
-	binds := []string{fmt.Sprintf("%s:%s", localFolder, "/opt/code")}
-
-	config := &container.Config{
-		Image: r.DockerImage,
-	}
-	hostConfig := &container.HostConfig{
-		Binds: binds,
-	}
-	networkingConfig := &network.NetworkingConfig{}
-	containerName := ""
-
-	container, err := cli.ContainerCreate(context.Background(), config, hostConfig, networkingConfig, containerName)
-	if err != nil {
-		log.WithField("err", err).Error("Error creating container")
-		return err
-	}
-	defer cli.ContainerRemove(context.Background(), types.ContainerRemoveOptions{ContainerID: container.ID})
-
-	log.WithField("id", container.ID).Error("Created container")
-
-	if err := cli.ContainerStart(context.Background(), container.ID); err != nil {
-		log.WithField("err", err).Error("ContainerStart")
-		return err
-	}
-
-	status, err := cli.ContainerWait(context.Background(), container.ID)
-	if err != nil {
-		return err
-	}
-
-	if status != 0 {
-		log.WithField("status", status).Error("Container exit status non zero")
-
-		options := types.ContainerLogsOptions{
-			ContainerID: container.ID,
-			ShowStdout:  true,
-			ShowStderr:  true,
-			Follow:      true,
-			Tail:        "all",
-		}
-		reader, err := cli.ContainerLogs(context.Background(), options)
-		if err != nil {
-			log.WithField("err", err).Error("ContainerLogs")
-			return err
-		}
-		defer reader.Close()
-
-		if res, err := ioutil.ReadAll(reader); err == nil {
-			log.Error(string(res))
-		} else {
-			log.WithField("err", err).Warn("failed to get logs")
-		}
-
-		return errors.New("Returned non zero status")
-	}
-
-	return nil
-}
-
-// rump special json
-func ToRumpJson(c RumpConfig) (string, error) {
-
-	blk := c.Blk
-	c.Blk = nil
-
-	jsonConfig, err := json.Marshal(c)
-	if err != nil {
-		return "", err
-	}
-
-	blks := ""
-	for _, b := range blk {
-
-		blkjson, err := json.Marshal(b)
-		if err != nil {
-			return "", err
-		}
-		blks += fmt.Sprintf("\"blk\": %s,", string(blkjson))
-	}
-	var jsonString string
-	if len(blks) > 0 {
-
-		jsonString = string(jsonConfig[:len(jsonConfig)-1]) + "," + blks[:len(blks)-1] + "}"
-
-	} else {
-		jsonString = string(jsonConfig)
-	}
-
-	return jsonString, nil
-
+	return RunContainer(r.DockerImage, nil, []string{fmt.Sprintf("%s:%s", localFolder, "/opt/code")}, false)
 }
