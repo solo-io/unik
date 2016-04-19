8b28cecf03e0fbbd23fdb17cf826699bf32d8335
Scott Weiss <scott.weiss@emc.com>
2016-04-19 00:11:04 -0400

    fixes to vbox
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/providers/virtualbox/virtualboxclient/virtualbox_client.go
+++ pkg/providers/virtualbox/virtualboxclient/virtualbox_client.go
@@ -4,13 +4,17 @@ import (
 	"github.com/layer-x/layerx-commons/lxerrors"
 	"os/exec"
 	"strings"
-	"github.com/pwnall/vbox"
-	"net/url"
 	"fmt"
-	"time"
 	"github.com/Sirupsen/logrus"
-	uniklog "github.com/emc-advanced-dev/unik/pkg/util/log"
+	"path/filepath"
+	"regexp"
 )
+
+type VboxVm struct {
+	Name string
+	MACAddr string
+	Running bool
+}
 
 func vboxManage(args ...string) (string, error) {
 	cmd := exec.Command("VBoxManage", args...)
@@ -22,24 +26,94 @@ func vboxManage(args ...string) (string, error) {
 	return string(out), nil
 }
 
-func Vms() ([]vbox.Machine, error) {
-	vms, err := vbox.GetMachines()
+//for vms with virtualbox guest additions
+func GetVmIp(vmName string) (string, error) {
+	out, err := vboxManage("guestproperty", "get", vmName, "/VirtualBox/GuestInfo/Net/0/V4/IP")
+	if err != nil {
+		return "", lxerrors.New("retrieving vm ip", err)
+	}
+	if strings.Contains(string(out), "No value set") {
+		return "", lxerrors.New("ip property not available for this vm", nil)
+	}
+	r, err := regexp.Compile("([0-9]{1,3}[\\.]){3}[0-9]{1,3}")
+	if err != nil {
+		return "", lxerrors.New("compiling regex", err)
+	}
+	ipAddr := r.Find(out)
+	if ipAddr == nil {
+		return "", lxerrors.New("ip address not found in string "+string(out), nil)
+	}
+	return ipAddr, nil
+}
+
+func parseVmInfo(vmInfo string) (*VboxVm, error) {
+	rLineBegin, err := regexp.Compile("NIC 1:.*MAC. ")
+	if err != nil {
+		return nil, lxerrors.New("compiling regex", err)
+	}
+	rLineEnd, err := regexp.Compile(",.*")
+	if err != nil {
+		return nil, lxerrors.New("compiling regex", err)
+	}
+	var macAddr string
+	var running bool
+	lines := strings.Split(vmInfo, "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "MAC") {
+			macAddr = rLineBegin.ReplaceAll(rLineEnd.ReplaceAll([]byte(line), ""), "")
+			logrus.Debugf("mac address found for vm: %s", macAddr)
+		}
+		if strings.Contains(line, "State") && strings.Contains(line, "running") {
+			running = true
+		}
+	}
+	if macAddr == "" {
+		return nil, lxerrors.New("mac address not found in vm info: "+string(vmInfo), nil)
+	}
+	return &VboxVm{ MACAddr: macAddr, Running: running}, nil
+}
+
+func Vms() ([]*VboxVm, error) {
+	out, err := vboxManage("list", "vms")
 	if err != nil {
 		return nil, lxerrors.New("getting vm list from virtualbox", err)
 	}
+	vmNames := []string{}
+	lines := strings.Split(string(out), "\n")
+	r, err := regexp.Compile("\"(.*)\"")
+	if err != nil {
+		return nil, lxerrors.New("compiling regex", err)
+	}
+	for _, line := range lines {
+		vmName := r.FindStringSubmatch(line)
+		if len(vmName) > 0 {
+			vmNames = append(vmNames, vmName[1])
+		}
+	}
+	vms := []*VboxVm{}
+	for _, vmName := range vmNames {
+		vmInfo, err := vboxManage("showvminfo", vmName)
+		if err != nil {
+			return nil, lxerrors.New("getting vm info for "+vmName, err)
+		}
+		vm, err := parseVmInfo(string(vmInfo))
+		if err != nil {
+			return nil, lxerrors.New("parsing vm info string", err)
+		}
+		vm.Name = vmName
+		vms = append(vms, vm)
+	}
+
 	return vms, nil
 }
 
-func CreateVm(vmName string) error {
-	vm, err := vbox.CreateMachine(vmName, "Other", "")
-	if err != nil {
+func CreateVm(vmName, baseFolder, bridgeName, string, bridgeAdapterKey int) error {
+	if _, err := vboxManage("createvm", "--name", vmName, "--basefolder", baseFolder, "-ostype", "Linux26_64"); err != nil {
 		return lxerrors.New("creating vm", err)
 	}
-	defer vm.Release()
-	if err = vm.Register(); err != nil {
+	if _, err := vboxManage("registervm", filepath.Join(baseFolder, vmName, fmt.Sprintf("%s.vbox", vmName))); err != nil {
 		return lxerrors.New("registering vm", err)
 	}
-
 	if _, err := vboxManage("storagectl", vmName, "--name", "SCSI", "--add", "scsi", "--controller", "LsiLogic"); err != nil {
 		return lxerrors.New("adding scsi storage controller", err)
 	}
@@ -47,154 +121,32 @@ func CreateVm(vmName string) error {
 	if _, err := vboxManage("storagectl", vmName, "--name", "SCSI", "--add", "scsi", "--controller", "LsiLogic"); err != nil {
 		return lxerrors.New("adding scsi storage controller", err)
 	}
-	if _, err := vboxManage("modifyvm", vmName, "--nic1", "bridged", "--bridgeadapter1", c.Bridge, "--nictype1", "virtio"); err != nil {
+	if _, err := vboxManage("modifyvm", vmName, "--nic1", "bridged", fmt.Sprintf("%s%v", "--bridgeadapter", bridgeAdapterKey), bridgeName, "--nictype1", "virtio"); err != nil {
 		return lxerrors.New("setting bridged networking on vm", err)
 	}
 	return nil
 }
 
 func DestroyVm(vmName string) error {
-	vm, err := vbox.FindMachine(vmName)
-	if err != nil {
-		return lxerrors.New("finding machine "+vmName, err)
-	}
-	defer vm.Release()
-	media, err := vm.Unregister(vbox.CleanupMode_DetachAllReturnHardDisksOnly)
-	if err != nil {
-		return lxerrors.New("unregistering vm", err)
-	}
-	progress, err := vm.DeleteConfig(media)
-	if err != nil {
-		return lxerrors.New("deleting media", err)
-	}
-	if err = progress.WaitForCompletion(-1); err != nil; {
-		return lxerrors.New("waiting for delete media config to complete", err)
-	}
-	percent, err := progress.GetPercent()
-	if err != nil {
-		return lxerrors.New("could not get progress percent", err)
-	}
-	logrus.Debugf("finished deleting media config for %s: %v percent", vmName, percent)
-
-	if percent != 100 {
-		return lxerrors.New("config deletion stopped at "+fmt.Sprintf("%v", percent), err)
-	}
-	result, err := progress.GetResultCode()
-	if err != nil {
-		return lxerrors.New("getting result code from config deletion", err)
-	}
-	if result != 0 {
-		return lxerrors.New(fmt.Sprintf("config deletion failed with code %v", result), nil)
+	if _, err := vboxManage("unregistervm", vmName, "--delete"); err != nil {
+		return lxerrors.New("unregistering and deleting vm", err)
 	}
 	return nil
 }
 
 func PowerOnVm(vmName string) error {
-
-
-	machine, err := vbox.FindMachine(vmName)
-	if err != nil {
-		return lxerrors.New("finding machine "+vmName, err)
-	}
-	defer machine.Release()
-
-	session := vbox.Session{}
-	if err := session.Init(); err != nil {
-		return lxerrors.New("session init", err)
-	}
-	defer session.Release()
-
-	progress, err := machine.Launch(session, "gui", "");
-	if err != nil {
-		return lxerrors.New("launching vm session", err)
-	}
-	defer progress.Release()
-	defer func() {
-		if err = session.UnlockMachine(); err != nil {
-			logrus.WithError(err).Errorf("failed to unlock machine %s", vmName)
-			return
-		}
-		for {
-			state, err := session.GetState()
-			if err != nil {
-				logrus.WithError(err).Errorf("getting session state")
-				return
-			}
-			logrus.Debugf("Session state: %s for vm %s", state, vmName)
-			if state == vbox.SessionState_Unlocked {
-				break
-			}
-			time.Sleep(300 * time.Millisecond)
-		}
-		// TODO(pwnall): Figure out how to get rid of this timeout. The VM should
-		//     be unlocked, according to the check above, but unregistering the VM
-		//     fails if we don't wait.
-		time.Sleep(300 * time.Millisecond)
-	}()
-
-
-	if err = progress.WaitForCompletion(50000); err != nil {
-		return lxerrors.New("launching vm session", err)
-	}
-
-	console, err := session.GetConsole()
-	if err != nil {
-		return lxerrors.New("getting vm console", err)
-	}
-	defer console.Release()
-
-	console.PowerDown()
-
-	return nil
+	_, err := vboxManage("controlvm", vmName, "poweron")
+	return err
 }
 
 func PowerOffVm(vmName string) error {
-	cmd := exec.Command("docker", "run", "--rm",
-		"vsphere-client",
-		"govc",
-		"vm.power",
-		"--off=true",
-		"-k",
-		"-u", formatUrl(vc.u),
-		vmName,
-	)
-	logrus.WithFields(logrus.Fields{
-		"command": cmd.Args,
-	}).Debugf("running govc command")
-	uniklog.LogCommand(cmd, true)
-	err := cmd.Run()
-	if err != nil {
-		return lxerrors.New("failed running govc vm.power (off)", err)
-	}
-	return nil
+	_, err := vboxManage("controlvm", vmName, "poweroff")
+	return err
 }
 
-func AttachVmdk(vmName, vmdkPath string) error {
-	password, _ := vc.u.User.Password()
-	cmd := exec.Command("docker", "run", "--rm",
-		"vsphere-client",
-		"java",
-		"-jar",
-		"/vsphere-client.jar",
-		"VmAttachDisk",
-		vc.u.String(),
-		vc.u.User.Username(),
-		password,
-		vmName,
-		"[datastore1] " + vmdkPath,
-		"200", //TODO: is this right?
-	)
-	logrus.WithFields(logrus.Fields{
-		"command": cmd.Args,
-	}).Debugf("running vsphere-client.jar command")
-	uniklog.LogCommand(cmd, true)
-	err := cmd.Run()
-	if err != nil {
-		return lxerrors.New("failed running vsphere-client.jar AttachVmdk", err)
+func AttachDisk(vmName, vmdkPath string, controllerPort int) error {
+	if _, err := vboxManage("storageattach", vmName, "--storagectl", "SCSI", "--port", fmt.Sprintf("%v", controllerPort), "--type", "hdd", "--medium", vmdkPath); err != nil {
+		return lxerrors.New("attaching storage", err)
 	}
 	return nil
-}
-
-func formatUrl(u *url.URL) string {
-	return "https://" + strings.TrimPrefix(strings.TrimPrefix(u.String(), "http://"), "https://")
 }
