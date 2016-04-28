9914e6039f57200204f9dd442315d97e8879047f
Scott Weiss <scott.weiss@emc.com>
2016-04-28 15:36:44 -0400

    target command
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ cmd/target.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/emc-advanced-dev/unik/pkg/config"
+	"errors"
+	"io/ioutil"
+	"github.com/Sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var url string
+var port int
+var show bool
+
+var targetCmd = &cobra.Command{
+	Use:   "target",
+	Short: "Configure unik daemon URL for cli client commands",
+	Long: `Sets the url of the unik daemon for cli commands.
+	If running unik locally, say 'unik target -url localhost'
+
+	args:
+	--url: <string, required>: url/ip address of the host running the unik daemon
+	--port: <int, optional>: port the daemon is running on (default: 3000)
+
+	--show: <bool,optional>: shows the current target that is set`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if show {
+			readClientConfig()
+			logrus.Infof("Current target: %s", clientConfig.DaemonUrl)
+			return
+		}
+		if url == "localhost" {
+			url = "127.0.0.1"
+		}
+		if err := setClientConfig(url, port); err != nil {
+			logrus.WithError(err).Error("failed to save target to config file")
+		}
+		logrus.Infof("target set: %s:%v", url, port)
+	},
+}
+
+func setClientConfig(url string, port int) error {
+	data, err := yaml.Marshal(config.ClientConfig{DaemonUrl: url+fmt.Sprintf(":%v", port)})
+	if err != nil {
+		return errors.New("failed to convert config to yaml string: "+err.Error())
+	}
+	if err := ioutil.WriteFile(clientConfigFile, data, 0644); err !=nil {
+		return errors.New("failed writing config to file "+ clientConfigFile +": "+err.Error())
+	}
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(targetCmd)
+	targetCmd.Flags().StringVar(&url, "url", "localhost", "<string, required>: url/ip address of the host running the unik daemon")
+	targetCmd.Flags().IntVar(&port, "port", 3000, "<int, optional>: port the daemon is running on (default: 3000)")
+	targetCmd.Flags().BoolVar(&show, "show", false, "<bool,optional>: shows the current target that is set")
+}
