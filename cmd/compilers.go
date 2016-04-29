c3aa7aef951d65bb2011eb0b2f8eb7ec6e56a876
Scott Weiss <scott.weiss@emc.com>
2016-04-29 14:23:30 -0400

    list providers and compilers
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ cmd/compilers.go
@@ -0,0 +1,48 @@
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/Sirupsen/logrus"
+	"github.com/emc-advanced-dev/unik/pkg/client"
+	"os"
+	"fmt"
+	"strings"
+)
+
+// compilersCmd represents the compilers command
+var compilersCmd = &cobra.Command{
+	Use:   "compilers",
+	Short: "List available unikernel compilers",
+	Long: `Returns a list of compilers available to the targeted unik backend.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		readClientConfig()
+		if url == "" {
+			url = clientConfig.DaemonUrl
+		}
+		logrus.WithField("url", url).Info("listing compilers")
+		compilers, err := client.UnikClient(url).AvailableCompilers()
+		if err != nil {
+			logrus.WithError(err).Error("listing compilers failed")
+			os.Exit(-1)
+		}
+		fmt.Printf("%s\n", strings.Join(compilers, "\n"))
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(compilersCmd)
+}
---
+++ cmd/providers.go
@@ -0,0 +1,49 @@
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/Sirupsen/logrus"
+	"strings"
+	"os"
+	"github.com/emc-advanced-dev/unik/pkg/client"
+)
+
+// providersCmd represents the providers command
+var providersCmd = &cobra.Command{
+	Use:   "providers",
+	Short: "List available unikernel providers",
+	Long: `Returns a list of providers available to the targeted unik backend.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		readClientConfig()
+		if url == "" {
+			url = clientConfig.DaemonUrl
+		}
+		logrus.WithField("url", url).Info("listing providers")
+		providers, err := client.UnikClient(url).AvailableProviders()
+		if err != nil {
+			logrus.WithError(err).Error("listing providers failed")
+			os.Exit(-1)
+		}
+		fmt.Printf("%s\n", strings.Join(providers, "\n"))
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(providersCmd)
+}
