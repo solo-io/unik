7886a94f813cda1651faf2cdba52413a87fd1fd7
Scott Weiss <scott.weiss@emc.com>
2016-04-30 20:51:10 -0400

    describe image and instance
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ cmd/describe-image.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/Sirupsen/logrus"
+	"github.com/emc-advanced-dev/unik/pkg/client"
+	"os"
+	"encoding/json"
+	"errors"
+)
+
+var describeImageCmd = &cobra.Command{
+	Use:   "describe-image",
+	Short: "Get image info as a Json string",
+	Long: `Get a json representation of an image as it is stored in unik.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := func() error {
+			if err := readClientConfig(); err != nil {
+				return err
+			}
+			if host == "" {
+				host = clientConfig.Host
+			}
+			if name == "" {
+				return errors.New("must specify --image")
+			}
+			image, err := client.UnikClient(host).Images().Get(name)
+			if err != nil {
+				return err
+			}
+			data, err := json.Marshal(image)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s\n", string(data))
+			return nil
+		}(); err != nil {
+			logrus.Errorf("describing image failed: %v", err)
+			os.Exit(-1)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(describeImageCmd)
+	describeImageCmd.Flags().StringVar(&name, "image", "", "<string,required> name or id of image. unik accepts a prefix of the name or id")
+}
---
+++ cmd/describe-instance.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"os"
+	"github.com/Sirupsen/logrus"
+	"encoding/json"
+	"github.com/emc-advanced-dev/unik/pkg/client"
+	"errors"
+)
+
+var describeInstanceCmd = &cobra.Command{
+	Use:   "describe-instance",
+	Short: "Get instance info as a Json string",
+	Long: `Get a json representation of an instance as it is stored in unik.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := func() error {
+			if err := readClientConfig(); err != nil {
+				return err
+			}
+			if host == "" {
+				host = clientConfig.Host
+			}
+			if name == "" {
+				return errors.New("must specify --instance")
+			}
+			instance, err := client.UnikClient(host).Instances().Get(name)
+			if err != nil {
+				return err
+			}
+			data, err := json.Marshal(instance)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s\n", string(data))
+			return nil
+		}(); err != nil {
+			logrus.Errorf("failed describing instance: %v", err)
+			os.Exit(-1)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(describeInstanceCmd)
+	describeInstanceCmd.Flags().StringVar(&name, "instance", "", "<string,required> name or id of instance. unik accepts a prefix of the name or id")
+}
