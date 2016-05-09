679cb32eec1a4b3cc26419960f5755842d471907
Scott Weiss <scott.weiss@emc.com>
2016-05-09 17:14:20 -0400

    add compiler names to providers
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/compilers/names.go
+++ pkg/compilers/names.go
@@ -3,11 +3,14 @@ package compilers
 const (
 	//available compilers
 	RUMP_GO_AWS = "rump-go-aws"
-	RUMP_GO_VMWARE = "rump-go-vmware"
 	RUMP_GO_VIRTUALBOX = "rump-go-virtualbox"
+	RUMP_GO_VMWARE = "rump-go-vmware"
+
+	RUMP_NODEJS_AWS = "rump-nodejs-aws"
 	RUMP_NODEJS_VIRTUALBOX = "rump-nodejs-virtualbox"
+	RUMP_NODEJS_VMWARE = "rump-nodejs-vmware"
 
 	OSV_JAVA_AWS = "osv-java-aws"
-	OSV_JAVA_VMAWRE = "osv-java-vmware"
 	OSV_JAVA_VIRTUALBOX = "osv-java-virtualbox"
+	OSV_JAVA_VMAWRE = "osv-java-vmware"
 )
