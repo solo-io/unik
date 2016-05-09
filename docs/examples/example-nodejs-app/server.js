699d40d1ca46f86101e67bbe10994db4a54c8bc9
Scott Weiss <scott.weiss@emc.com>
2016-05-09 12:59:29 -0400

    example nmode project
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ docs/examples/example-nodejs-app/server.js
@@ -0,0 +1,42 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+
+//Lets define a port we want to listen to
+const PORT=8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
