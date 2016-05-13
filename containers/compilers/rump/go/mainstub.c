ef81cfda9d544a430723be867ccf678ceee947fb
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-05-14 02:01:07 +0300

    this hack makes it impossible to run multiple programs. need to figure out how to solve it.

--- containers/compilers/rump/go/mainstub.c
+++ containers/compilers/rump/go/mainstub.c
@@ -1,6 +1,8 @@
 
 int main() {
-// 	rump_pub_lwproc_releaselwp(); /* XXX */
+ 	// rump_pub_lwproc_releaselwp(); 
+	 /* see ultimate kludge: https://github.com/deferpanic/gorump/commit/ab63d5a1389aba2a588dbc2a956b1ea97e27dc53 
+	 do not use this work around if you expect your program to exit, as it will panic*/
 	gomaincaller();
 	return 0;
 }
