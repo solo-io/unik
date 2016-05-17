1f97a840a3c830261b7ed2dae0b64eee933cbec7
Scott Weiss <scott.weiss@emc.com>
2016-05-17 14:00:47 -0400

    remove RunContainer docker api
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/compilers/rump/generic.go
+++ pkg/compilers/rump/generic.go
@@ -6,17 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
-
-	log "github.com/Sirupsen/logrus"
-
-	"github.com/docker/engine-api/client"
-	"github.com/docker/engine-api/types"
-	"github.com/docker/engine-api/types/container"
-	"github.com/docker/engine-api/types/network"
-	"github.com/docker/engine-api/types/strslice"
 	unikos "github.com/emc-advanced-dev/unik/pkg/os"
 	unikutil "github.com/emc-advanced-dev/unik/pkg/util"
-	"golang.org/x/net/context"
 	"os/exec"
 	"strings"
 )
@@ -54,72 +45,6 @@ func BuildBootableImage(kernel, cmdline string) (string, error) {
 	return resultFile.Name(), nil
 }
 
-func RunContainer(imageName string, cmds, binds []string, privileged bool, envPairs []string) error {
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		return err
-	}
-
-	config := &container.Config{
-		Image: imageName,
-		Cmd:   strslice.StrSlice(cmds),
-		Env: envPairs,
-	}
-	hostConfig := &container.HostConfig{
-		Binds:      binds,
-		Privileged: privileged,
-	}
-	networkingConfig := &network.NetworkingConfig{}
-	containerName := ""
-
-	container, err := cli.ContainerCreate(context.Background(), config, hostConfig, networkingConfig, containerName)
-	if err != nil {
-		log.WithField("err", err).Error("Error creating container")
-		return err
-	}
-	defer cli.ContainerRemove(context.Background(), types.ContainerRemoveOptions{ContainerID: container.ID})
-
-	log.WithFields(log.Fields{"id": container.ID, "cmd": cmds, "binds": binds}).Info("Created container")
-
-	if err := cli.ContainerStart(context.Background(), container.ID); err != nil {
-		log.WithField("err", err).Error("ContainerStart")
-		return err
-	}
-
-	status, err := cli.ContainerWait(context.Background(), container.ID)
-	if err != nil {
-		return err
-	}
-
-	if status != 0 {
-		log.WithField("status", status).Error("Container exit status non zero")
-
-		options := types.ContainerLogsOptions{
-			ContainerID: container.ID,
-			ShowStdout:  true,
-			ShowStderr:  true,
-			Follow:      true,
-			Tail:        "all",
-		}
-		reader, err := cli.ContainerLogs(context.Background(), options)
-		if err != nil {
-			log.WithField("err", err).Error("ContainerLogs")
-			return err
-		}
-		defer reader.Close()
-
-		if res, err := ioutil.ReadAll(reader); err == nil {
-			log.Error(string(res))
-		} else {
-			log.WithField("err", err).Warn("failed to get logs")
-		}
-
-		return errors.New("Returned non zero status", nil)
-	}
-
-	return nil
-}
-
 func execContainer(imageName string, cmds, binds []string, privileged bool, env map[string]string) error {
 	dockerArgs := []string{"run", "--rm"}
 	if privileged {
@@ -142,7 +67,6 @@ func execContainer(imageName string, cmds, binds []string, privileged bool, env 
 }
 
 func (r *RumpGoCompiler) runContainer(localFolder string, envPairs []string) error {
-	//return RunContainer(r.DockerImage, nil, []string{fmt.Sprintf("%s:%s", localFolder, "/opt/code")}, false, envPairs)
 	env := make(map[string]string)
 	for _, pair := range envPairs {
 		split := strings.Split(pair, "=")
