27d389b5a61e86297d59943b4dd9d73a02c94da0
Scott Weiss <scott.weiss@emc.com>
2016-05-18 09:21:16 -0400

    include godeps in new instance listener fixes #27 Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/providers/common/compile_instance_listener.go
+++ pkg/providers/common/compile_instance_listener.go
@@ -7,6 +7,7 @@ import (
 	"github.com/emc-advanced-dev/unik/pkg/compilers/rump"
 	"github.com/emc-advanced-dev/unik/instance-listener/bindata"
 	"github.com/emc-advanced-dev/unik/pkg/types"
+	"os"
 )
 
 func CompileInstanceListener(sourceDir, instanceListenerPrefix, dockerImage string, createImageFunc func(kernel, args string, mntPoints []string) (*types.RawImage, error)) (*types.RawImage, error) {
@@ -16,6 +17,16 @@ func CompileInstanceListener(sourceDir, instanceListenerPrefix, dockerImage stri
 	}
 	if err := ioutil.WriteFile(filepath.Join(sourceDir, "main.go"), mainData, 0644); err != nil {
 		return nil, errors.New("copying contents of instance listener main.go", err)
+	}
+	godepsData, err := bindata.Asset("instance-listener/Godeps/Godeps.json")
+	if err != nil {
+		return nil, errors.New("reading binary data of instance listener Godeps", err)
+	}
+	if err := os.MkdirAll(filepath.Join(sourceDir, "Godeps"), 0755); err != nil {
+		return nil, errors.New("creating Godeps dir", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(sourceDir, "Godeps", "Godeps.json"), godepsData, 0644); err != nil {
+		return nil, errors.New("copying contents of instance listener Godeps.json", err)
 	}
 
 	params := types.CompileImageParams{
