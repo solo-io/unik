f6532e61d77478d3ba477e77a92a3f24b972ae23
Scott Weiss <scott.weiss@emc.com>
2016-05-23 00:47:43 -0400

    install vbox on start, add a real test
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -5,13 +5,31 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/emc-advanced-dev/unik/test/helpers"
+	"github.com/kubernetes/kubernetes/Godeps/_workspace/src/github.com/Sirupsen/logrus"
+	"github.com/emc-advanced-dev/unik/pkg/daemon"
 )
 
 var _ = Describe("Client", func() {
-	It("runs the test", func(){
-		c := UnikClient("127.0.0.1")
-		images, err := c.Images().All()
-		Expect(err).To(HaveOccurred())
-		Expect(images).To(BeNil())
+	var d *daemon.UnikDaemon
+	BeforeEach(func(){
+		var err error
+		d, err = helpers.DaemonFromEnv()
+		if err != nil {
+			logrus.Fatalf(err)
+		}
+	})
+	AfterEach(func(){
+		d.Stop()
+	})
+	Describe("images", func(){
+		Describe("All()", func(){
+			It("returns a list of images", func(){
+				c := UnikClient("127.0.0.1:3000")
+				images, err := c.Images().All()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(images).To(BeEmpty())
+			})
+		})
 	})
 })
---
+++ test/helpers/test_helpers.go
@@ -0,0 +1,22 @@
+package helpers
+
+import (
+	"github.com/emc-advanced-dev/unik/pkg/daemon"
+	"gopkg.in/yaml.v2"
+	"os"
+	"github.com/emc-advanced-dev/unik/pkg/config"
+	"github.com/emc-advanced-dev/pkg/errors"
+)
+
+func DaemonFromEnv() (*daemon.UnikDaemon, error) {
+	var daemonConfig config.DaemonConfig
+	daemonConfigString := os.Getenv("DAEMON_CONFIG")
+	if err := yaml.Unmarshal([]byte(daemonConfigString), &daemonConfig); err != nil {
+		return nil, errors.New("not valid yaml: "+daemonConfigString, err)
+	}
+	d, err := daemon.NewUnikDaemon(daemonConfig)
+	if err != nil {
+		return nil, errors.New("daemon failed to initialize", err)
+	}
+	return d, nil
+}
