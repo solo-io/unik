88b608ba08f6839e8a6e0265ea951fdeacbd7c87
Scott Weiss <scott.weiss@emc.com>
2016-05-23 14:44:09 -0400

    daemonread from a file
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- test/helpers/test_helpers.go
+++ test/helpers/test_helpers.go
@@ -9,13 +9,22 @@ import (
 	"path/filepath"
 	"os/exec"
 	"github.com/emc-advanced-dev/unik/pkg/util"
+	"io/ioutil"
 )
 
 func DaemonFromEnv() (*daemon.UnikDaemon, error) {
 	var daemonConfig config.DaemonConfig
-	daemonConfigString := os.Getenv("DAEMON_CONFIG")
-	if err := yaml.Unmarshal([]byte(daemonConfigString), &daemonConfig); err != nil {
-		return nil, errors.New("not valid yaml: "+daemonConfigString, err)
+	var data []byte
+	daemonConfigFile := os.Getenv("DAEMON_CONFIG_FILE")
+	if daemonConfigFile == "" {
+		daemonConfigFile = os.Getenv("HOME")+"/.unik/daemon-config.yaml"
+	}
+	data, err := ioutil.ReadFile(daemonConfigFile)
+	if err != nil {
+		return nil, errors.New("failed to read "+daemonConfigFile, err)
+	}
+	if err := yaml.Unmarshal(data, &daemonConfig); err != nil {
+		return nil, errors.New("not valid yaml: "+ daemonConfigFile, err)
 	}
 	d, err := daemon.NewUnikDaemon(daemonConfig)
 	if err != nil {
