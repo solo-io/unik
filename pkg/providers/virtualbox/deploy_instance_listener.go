659a9b568e44a609b34e529b45f0076868859cb4
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-05-24 13:02:54 +0300

    typo fix

--- pkg/providers/virtualbox/deploy_instance_listener.go
+++ pkg/providers/virtualbox/deploy_instance_listener.go
@@ -1,18 +1,19 @@
 package virtualbox
 
 import (
-	"github.com/Sirupsen/logrus"
-	unikos "github.com/emc-advanced-dev/unik/pkg/os"
-	"github.com/emc-advanced-dev/unik/pkg/providers/virtualbox/virtualboxclient"
-	"github.com/emc-advanced-dev/pkg/errors"
-	"os"
-	unikutil "github.com/emc-advanced-dev/unik/pkg/util"
-	"github.com/emc-advanced-dev/unik/pkg/config"
-	"github.com/emc-advanced-dev/unik/pkg/types"
-	"github.com/emc-advanced-dev/unik/pkg/providers/common"
-	"time"
 	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/emc-advanced-dev/pkg/errors"
 	"github.com/emc-advanced-dev/unik/pkg/compilers/rump"
+	"github.com/emc-advanced-dev/unik/pkg/config"
+	unikos "github.com/emc-advanced-dev/unik/pkg/os"
+	"github.com/emc-advanced-dev/unik/pkg/providers/common"
+	"github.com/emc-advanced-dev/unik/pkg/providers/virtualbox/virtualboxclient"
+	"github.com/emc-advanced-dev/unik/pkg/types"
+	unikutil "github.com/emc-advanced-dev/unik/pkg/util"
 )
 
 var timeout = time.Second * 10
@@ -40,13 +41,13 @@ func (p *VirtualboxProvider) DeployInstanceListener(config config.Virtualbox) er
 	logrus.Infof("staging new instance listener image")
 	os.RemoveAll(getImagePath(VboxUnikInstanceListener))
 	params := types.StageImageParams{
-		Name: VboxUnikInstanceListener,
+		Name:     VboxUnikInstanceListener,
 		RawImage: rawImage,
-		Force: true,
+		Force:    true,
 	}
 	image, err := p.Stage(params)
 	if err != nil {
-		return errors.New("building bootable vsphere image for instsance listener", err)
+		return errors.New("building bootable virtualbox image for instsance listener", err)
 	}
 	defer func() {
 		if err != nil {
@@ -75,7 +76,7 @@ func (p *VirtualboxProvider) runInstanceListener(image *types.Image) (err error)
 		}
 		defer os.Remove(imagePath)
 		createVolumeParams := types.CreateVolumeParams{
-			Name: instanceListenerData,
+			Name:      instanceListenerData,
 			ImagePath: imagePath,
 		}
 
@@ -89,7 +90,6 @@ func (p *VirtualboxProvider) runInstanceListener(image *types.Image) (err error)
 			}
 		}()
 	}
-
 
 	instanceDir := getInstanceDir(VboxUnikInstanceListener)
 	defer func() {
@@ -105,7 +105,7 @@ func (p *VirtualboxProvider) runInstanceListener(image *types.Image) (err error)
 		}
 	}()
 
-	logrus.Debugf("creating vsphere vm")
+	logrus.Debugf("creating virtualbox vm")
 
 	if err := virtualboxclient.CreateVm(VboxUnikInstanceListener, virtualboxInstancesDirectory, image.RunSpec.DefaultInstanceMemory, p.config.AdapterName, p.config.VirtualboxAdapterType, image.RunSpec.StorageDriver); err != nil {
 		return errors.New("creating vm", err)
@@ -150,14 +150,14 @@ func (p *VirtualboxProvider) runInstanceListener(image *types.Image) (err error)
 		return errors.New("powering on vm", err)
 	}
 
-	instanceListenerIp, err := common.GetInstanceListenerIp(instanceListenerPrefix, time.Second * 30)
+	instanceListenerIp, err := common.GetInstanceListenerIp(instanceListenerPrefix, time.Second*30)
 	if err != nil {
 		return errors.New("failed to retrieve instance listener ip. is unik instance listener running?", err)
 	}
 
 	vm, err := virtualboxclient.GetVm(VboxUnikInstanceListener)
 	if err != nil {
-		return errors.New("getting vm info from vsphere", err)
+		return errors.New("getting vm info from virtualbox", err)
 	}
 
 	instanceId := vm.UUID
@@ -166,7 +166,7 @@ func (p *VirtualboxProvider) runInstanceListener(image *types.Image) (err error)
 		Name:           VboxUnikInstanceListener,
 		State:          types.InstanceState_Pending,
 		IpAddress:      instanceListenerIp,
-		Infrastructure: types.Infrastructure_VSPHERE,
+		Infrastructure: types.Infrastructure_VIRTUALBOX,
 		ImageId:        image.Id,
 		Created:        time.Now(),
 	}
