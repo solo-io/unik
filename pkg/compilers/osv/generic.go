c2753a269f5f9f13fdc58f9a0d3e2782719d0980
Scott Weiss <scott.weiss@emc.com>
2016-06-15 14:50:56 -0400

    osv copiler reconfigured to use jar container
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/compilers/osv/generic.go
+++ pkg/compilers/osv/generic.go
@@ -9,16 +9,47 @@ import (
 	"github.com/emc-advanced-dev/pkg/errors"
 	"github.com/emc-advanced-dev/unik/pkg/types"
 	unikutil "github.com/emc-advanced-dev/unik/pkg/util"
+	"gopkg.in/yaml.v2"
+	"strings"
 )
+
+
+type javaProjectConfig struct {
+	MainClassName string `yaml:"main_class_name"`
+	GroupId string `yaml:"artifact_group_id"`
+	ArtifactId string `yaml:"artifact_id"`
+	Version string `yaml:"artifact_version"`
+}
 
 func compileRawImage(params types.CompileImageParams, useEc2Bootstrap bool) (string, error) {
 	sourcesDir := params.SourcesDir
+
+	var config javaProjectConfig
+	data, err := ioutil.ReadFile(filepath.Join(sourcesDir, "manifest.yaml"))
+	if err != nil {
+		return nil, errors.New("failed to read manifest.yaml file", err)
+	}
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, errors.New("failed to parse yaml manifest.yaml file", err)
+	}
 
 	container := unikutil.NewContainer("compilers-osv-java").WithVolume("/dev", "/dev").WithVolume(sourcesDir+"/", "/project_directory")
 	var args []string
 	if useEc2Bootstrap {
 		args = append(args, "-ec2", "true")
 	}
+
+	jarName, err := getJarNmae(sourcesDir)
+	if err != nil {
+		return "", errors.New("failed to get jar name", err)
+	}
+
+	args = append(args, "-jarName", jarName)
+	args = append(args, "-args", params.Args)
+	args = append(args, "-groupId", config.GroupId)
+	args = append(args, "-artifactId", config.ArtifactId)
+	args = append(args, "-version", config.Version)
+	args = append(args, "-mainClassName", config.MainClassName)
 
 	logrus.WithFields(logrus.Fields{
 		"args": args,
@@ -42,4 +73,20 @@ func compileRawImage(params types.CompileImageParams, useEc2Bootstrap bool) (str
 		return "", errors.New("failed to rename result file", err)
 	}
 	return resultFile.Name(), nil
+}
+
+func getJarNmae(projectDir string) (string, error) {
+	var jarName string
+	if err := filepath.Walk(projectDir, func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(info.Name(), ".jar") || strings.Contains(info.Name(), ".war") {
+			jarName = info.Name()
+		}
+		return nil
+	}); err != nil {
+		return "", err
+	}
+	if jarName == "" {
+		return "", errors.New("could not find .jar or .war file", nil)
+	}
+	return jarName, nil
 }
