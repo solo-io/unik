d80cce95fc538e67f41757fd5313cdb179893caf
Scott Weiss <scott.weiss@emc.com>
2016-06-15 14:50:39 -0400

    boot creator looks for a jar rather than a maven project
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- containers/compilers/osv/java-compiler/osv_boot_creator.go
+++ containers/compilers/osv/java-compiler/osv_boot_creator.go
@@ -10,6 +10,8 @@ import (
 	"time"
 	"path/filepath"
 	"flag"
+	"io/ioutil"
+	"github.com/emc-advanced-dev/pkg/errors"
 )
 
 //expect project directory at /project_directory; mount w/ -v FOLDER:/project_directory
@@ -25,6 +27,12 @@ var buildImageTimeout = time.Minute * 10
 
 func main() {
 	useEc2Bootstrap := flag.Bool("ec2", false, "indicates whether to compile using the wrapper for ec2")
+	groupId := flag.String("groupId", "", "groupid for jar file")
+	artifactId := flag.String("artifactId", "", "artifactid for jar file")
+	version := flag.String("version", "", "artifactid for jar file")
+	mainClassName := flag.String("mainClassName", "", "mainClassName for jar file")
+	jarName := flag.String("jarName", "", "name of jar file (not path)")
+	args := flag.String("args", "", "arguments to kernel")
 	flag.Parse()
 	javaMainCallerDir := java_main_caller_udp_bootstrap_dir //use udp by default
 	if *useEc2Bootstrap {
@@ -32,43 +40,37 @@ func main() {
 	}
 	out, _ := exec.Command("ls", "/").CombinedOutput()
 	fmt.Println(strings.Split(string(out), "\n"))
-	appInfo, err := wrapJavaApplication(javaMainCallerDir, project_directory)
-	if err != nil {
+	info := appInfo{
+		groupId: *groupId,
+		artifactId: *artifactId,
+		version: *version,
+		mainClassName: *mainClassName,
+	}
+	if err := wrapJavaApplication(info, javaMainCallerDir, project_directory); err != nil {
 		logrus.WithError(err).Errorf("Failed to wrap java project Main Class", err)
 		os.Exit(-1)
 	}
 	logrus.AddHook(&unikutil.AddTraceHook{true})
-	fmt.Printf("read info from java project: %v\n", appInfo)
-
-	fmt.Printf("runnning mvn clean")
-	mvnClean := exec.Command("mvn", "clean")
-	mvnClean.Dir = project_directory
-	if err := mvnClean.Run(); err != nil {
-		fmt.Printf("mvn clean failed, simply cleaning up %s/target", project_directory)
-		os.RemoveAll(filepath.Join(project_directory, "target"))
-	}
-
-	fmt.Printf("running mvn package\n")
-
-	mvnPackageCmd := exec.Command("mvn", "package")
-	mvnPackageCmd.Dir = project_directory
-	printCommand(mvnPackageCmd)
-	if out, err := mvnPackageCmd.CombinedOutput(); err != nil {
-		logrus.WithError(err).Error(string(out))
-		os.Exit(-1)
-	}
 
 	mvnInstallCmd := exec.Command("mvn", "install:install-file",
-		"-Dfile=target/" + appInfo.ArtifactId + "-" + appInfo.Version + "-jar-with-dependencies.jar",
-		"-DgroupId=" + appInfo.GroupId,
-		"-DartifactId=" + appInfo.ArtifactId,
-		"-Dversion=" + appInfo.Version,
+		"-Dfile="+filepath.Join(project_directory, *jarName),
+		"-DgroupId=" + appInfo.groupId,
+		"-DartifactId=" + appInfo.artifactId,
+		"-Dversion=" + appInfo.version,
 		"-Dpackaging=jar")
 	mvnInstallCmd.Dir = project_directory
 	printCommand(mvnInstallCmd)
 	if out, err := mvnInstallCmd.CombinedOutput(); err != nil {
 		logrus.WithError(err).Error(string(out))
 		os.Exit(-1)
+	}
+
+	//add args to capstanfile
+	if *args != "" {
+		if err := addArgs(filepath.Join(javaMainCallerDir, "Capstanfile"), *args); err != nil {
+			logrus.WithError(err).Error("adding capstan args failed")
+			os.Exit(-1)
+		}
 	}
 
 	go func() {
@@ -80,7 +82,7 @@ func main() {
 		capstanCmd.Stderr = os.Stderr
 		printCommand(capstanCmd)
 		if err := capstanCmd.Run(); err != nil {
-			logrus.WithError(err).Error("captsain build failed")
+			logrus.WithError(err).Error("capstan build failed")
 			os.Exit(-1)
 		}
 	}()
@@ -135,4 +137,54 @@ func fileReady(filename string) <-chan struct{} {
 
 func printCommand(cmd *exec.Cmd) {
 	fmt.Printf("running command from dir %s: %v\n", cmd.Dir, cmd.Args)
+}
+
+func addArgs(capstanFile, args string) error {
+	logrus.Infof("adding %s to args", *args)
+	capstanBytes, err := ioutil.ReadFile(capstanFile)
+	if err != nil {
+		return errors.New("reading capstanfile", err)
+	}
+	capstanContents := strings.Replace(string(capstanBytes), "cmdline: /java.so -jar /program.jar", fmt.Sprintf("cmdline: /java.so -jar /program.jar %s", args), -1)
+
+	err = ioutil.WriteFile(capstanFile, []byte(capstanContents), 0666)
+	if err != nil {
+		return errors.New("writing capstanfile", err)
+	}
+	return nil
+}
+
+type appInfo struct {
+	groupId       string
+	artifactId    string
+	version       string
+	mainClassName string
+}
+
+func wrapJavaApplication(info appInfo, javaWrapperDir, appSourceDir string) error {
+	wrapperPomBytes, err := ioutil.ReadFile(javaWrapperDir + "/pom.xml")
+	if err != nil {
+		return errors.New("reading app pom bytes", err)
+	}
+	wrapperPomContents := strings.Replace(string(wrapperPomBytes), "REPLACE_WITH_GROUPID", info.groupId, -1)
+	wrapperPomContents = strings.Replace(wrapperPomContents, "REPLACE_WITH_ARTIFACTID", info.artifactId, -1)
+	wrapperPomContents = strings.Replace(wrapperPomContents, "REPLACE_WITH_VERSION", info.version, -1)
+
+	err = ioutil.WriteFile(javaWrapperDir + "/pom.xml", []byte(wrapperPomContents), 0666)
+	if err != nil {
+		return errors.New("writing pom.xml", err)
+	}
+
+	wrapperMainContentBytes, err := ioutil.ReadFile(javaWrapperDir + "/src/main/java/com/emc/wrapper/Wrapper.java")
+	if err != nil {
+		return errors.New("reading java pom bytes", err)
+	}
+	wrapperMainContents := strings.Replace(string(wrapperMainContentBytes), "REPLACE_WITH_MAIN_CLASS", info.mainClassName, -1)
+
+	err = ioutil.WriteFile(javaWrapperDir + "/src/main/java/com/emc/wrapper/Wrapper.java", []byte(wrapperMainContents), 0666)
+	if err != nil {
+		return errors.New("writing Wrapper class around app class", err)
+	}
+
+	return nil
 }
