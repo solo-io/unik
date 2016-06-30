c7d237fa0fe02e9f400fff03e4ef1520cf299ed4
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-06-30 13:29:47 +0300

    update node state

--- pkg/providers/photon/list_instances.go
+++ pkg/providers/photon/list_instances.go
@@ -1,6 +1,10 @@
 package photon
 
-import "github.com/emc-advanced-dev/unik/pkg/types"
+import (
+	"github.com/emc-advanced-dev/pkg/errors"
+
+	"github.com/emc-advanced-dev/unik/pkg/types"
+)
 
 func (p *PhotonProvider) ListInstances() ([]*types.Instance, error) {
 	if len(p.state.GetInstances()) < 1 {
@@ -8,8 +12,37 @@ func (p *PhotonProvider) ListInstances() ([]*types.Instance, error) {
 	}
 
 	var instances []*types.Instance
-	for _, v := range p.state.GetInstances() {
-		instances = append(instances, v)
+	for _, instance := range p.state.GetInstances() {
+
+		vm, err := p.client.VMs.Get(instance.Id)
+		if err != nil {
+			return nil, errors.New("retrieving vm for instance id "+instance.Id, err)
+		}
+
+		// TODO: get ip..
+
+		switch vm.State {
+		case "STARTED":
+			instance.State = types.InstanceState_Running
+		case "CREATING":
+			instance.State = types.InstanceState_Pending
+		case "STOPPED":
+			fallthrough
+		case "SUSPENDED":
+			fallthrough
+		default:
+			instance.State = types.InstanceState_Stopped
+			break
+		}
+		err = p.state.ModifyInstances(func(instances map[string]*types.Instance) error {
+			instances[instance.Id] = instance
+			return nil
+		})
+		if err != nil {
+			return nil, errors.New("saving instance to state", err)
+		}
+
+		instances = append(instances, instance)
 	}
 
 	return instances, nil
