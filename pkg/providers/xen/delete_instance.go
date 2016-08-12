ed7e18486b5d15c9149893f02d1b6f6933aaa49b
Scott Weiss <scott.weiss@emc.com>
2016-08-12 14:45:23 -0400

    implement deleteinstance
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/providers/xen/delete_instance.go
+++ pkg/providers/xen/delete_instance.go
@@ -1,5 +1,56 @@
-package qemu
+package xen
+
+import (
+	"github.com/emc-advanced-dev/pkg/errors"
+	"github.com/emc-advanced-dev/unik/pkg/types"
+	"os"
+)
 
 func (p *XenProvider) DeleteInstance(id string, force bool) error {
-	return p.StopInstance(id)
+	instance, err := p.GetInstance(id)
+	if err != nil {
+		return errors.New("retrieving instance "+id, err)
+	}
+
+	if err := p.client.DestroyVm(instance.Name); err != nil {
+		return errors.New("xen api destroy call", err)
+	}
+
+	if err := os.RemoveAll(getInstanceDir(instance.Name)); err != nil {
+		return errors.New("deleting instance dir", err)
+	}
+
+	volumesToDetach := []*types.Volume{}
+	volumes, err := p.ListVolumes()
+	if err != nil {
+		return errors.New("getting volume list", err)
+	}
+	for _, volume := range volumes {
+		if volume.Attachment == instance.Id {
+			volumesToDetach = append(volumesToDetach, volume)
+		}
+	}
+	if err := p.state.ModifyInstances(func(instances map[string]*types.Instance) error {
+		delete(instances, instance.Id)
+		return nil
+	}); err != nil {
+		return errors.New("modifying image map in state", err)
+	}
+	for _, volume := range volumesToDetach {
+		if err := p.state.ModifyVolumes(func(volumes map[string]*types.Volume) error {
+			volume, ok := volumes[volume.Id]
+			if !ok {
+				return errors.New("no record of "+volume.Id+" in the state", nil)
+			}
+			volume.Attachment = ""
+			return nil
+		}); err != nil {
+			return errors.New("modifying volume map in state", err)
+		}
+	}
+	err = p.state.Save()
+	if err != nil {
+		return errors.New("saving image map to state", err)
+	}
+	return nil
 }
