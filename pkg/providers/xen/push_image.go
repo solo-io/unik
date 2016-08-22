eda99ed0c9c5aa1c33b72f5fd2fc56eceb90a56c
Scott Weiss <scott.weiss@emc.com>
2016-08-22 17:08:37 -0400

    add qemu and xen support for pull/push
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/providers/xen/push_image.go
+++ pkg/providers/xen/push_image.go
@@ -1,10 +1,20 @@
 package xen
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/emc-advanced-dev/pkg/errors"
+	"github.com/emc-advanced-dev/unik/pkg/providers/common"
 	"github.com/emc-advanced-dev/unik/pkg/types"
 )
 
 func (p *XenProvider) PushImage(params types.PushImagePararms) error {
-	return errors.New("not implemented", nil)
+	image, err := p.GetImage(params.ImageName)
+	if err != nil {
+		return errors.New("finding image for "+params.ImageName, err)
+	}
+	if err := common.PushImage(params.Config, image, getImagePath(image.Name)); err != nil {
+		return errors.New("pushing image "+image.Name, err)
+	}
+	logrus.Infof("pushed image %v to %v", image.Name, params.Config.URL)
+	return nil
 }
