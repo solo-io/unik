a45d190d6b8dc6d7d2beea00627eaa563fcaaa22
Scott Weiss <scott.weiss@emc.com>
2016-09-01 17:31:46 -0400

    detect running qemu processes in qemu list instances
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- pkg/providers/qemu/list_instances.go
+++ pkg/providers/qemu/list_instances.go
@@ -1,6 +1,13 @@
 package qemu
 
-import "github.com/emc-advanced-dev/unik/pkg/types"
+import (
+	"fmt"
+	"github.com/emc-advanced-dev/pkg/errors"
+	"github.com/emc-advanced-dev/unik/pkg/types"
+	"os"
+	"strconv"
+	"syscall"
+)
 
 func (p *QemuProvider) ListInstances() ([]*types.Instance, error) {
 	if len(p.state.GetInstances()) < 1 {
@@ -8,9 +15,27 @@ func (p *QemuProvider) ListInstances() ([]*types.Instance, error) {
 	}
 
 	var instances []*types.Instance
-	for _, v := range p.state.GetInstances() {
-		instances = append(instances, v)
+	for _, instance := range p.state.GetInstances() {
+		pid, err := strconv.Atoi(instance.Id)
+		if err != nil {
+			return nil, errors.New("invalid id (is not a pid)", err)
+		}
+		if err := detectInstance(pid); err != nil {
+			p.state.RemoveInstance(instance)
+		}
+		instances = append(instances, instance)
 	}
 
 	return instances, nil
+}
+
+func detectInstance(pid int) error {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return errors.New("Failed to find process", err)
+	}
+	if err := process.Signal(syscall.Signal(0)); err != nil {
+		return errors.New(fmt.Sprintf("process.Signal on pid %d returned", pid), err)
+	}
+	return nil
 }
