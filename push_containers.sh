0f326c2e5bcb4df690152ad6008cc2b65abaa8c6
Scott Weiss <scott.weiss@emc.com>
2016-10-20 13:33:58 -0400

    make scripts executable
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- push_containers.sh
+++ push_containers.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 for i in $(printf "%s\n" $(cat containers/versions.json | jq -r 'keys[]')); do echo "pushing projectunik/$i:$(cat containers/versions.json  | jq .['$arg'] -r --arg arg $i)" && docker push projectunik/$i:$(cat containers/versions.json  | jq .['$arg'] -r --arg arg $i); done
