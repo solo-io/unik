c8c5f21ba45df2260bd7b8a4e8ec11d383bc8604
Scott Weiss <scott.weiss@emc.com>
2016-10-31 17:38:20 -0400

    bootstrap typ for gcloud
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

---
+++ containers/compilers/rump/go/stub/bootstrap_gcloud.go
@@ -0,0 +1,59 @@
+// +build: gcloud
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func bootstrap() error {
+	log.Printf("bootstrapping using gcloud metadata service")
+	env, err := getEnvGcloud()
+	if err != nil {
+		return errors.New("failed to get env from gcloud: " + err.Error())
+	}
+	if err := setEnv(env); err != nil {
+		return errors.New("setting env: " + err.Error())
+	}
+	return nil
+}
+
+var timeout = time.Duration(2 * time.Second)
+
+func dialTimeout(network, addr string) (net.Conn, error) {
+	return net.DialTimeout(network, addr, timeout)
+}
+
+func getEnvGcloud() (map[string]string, error) {
+	client := http.Client{
+		Transport: &http.Transport{
+			Dial: dialTimeout,
+		},
+	}
+	req, err := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ENV_DATA", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Metadata-Flavor", "Google")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var env map[string]string
+	if err := json.Unmarshal(data, &env); err != nil {
+		return nil, err
+	}
+	return env, nil
+}
---
+++ pkg/compilers/rump/rump-gcloud.go
@@ -0,0 +1,88 @@
+package rump
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/emc-advanced-dev/unik/pkg/compilers"
+	"github.com/emc-advanced-dev/unik/pkg/types"
+)
+
+func CreateImageGCloud(kernel string, args string, mntPoints, bakedEnv []string, noCleanup bool) (*types.RawImage, error) {
+	return createImageGCloud(kernel, args, mntPoints, bakedEnv, noCleanup, false)
+}
+
+func CreateImageGCloudAddStub(kernel string, args string, mntPoints, bakedEnv []string, noCleanup bool) (*types.RawImage, error) {
+	return createImageGCloud(kernel, args, mntPoints, bakedEnv, noCleanup, true)
+}
+
+func createImageGCloud(kernel string, args string, mntPoints, bakedEnv []string, noCleanup, addStub bool) (*types.RawImage, error) {
+	// create rump config
+	var c rumpConfig
+	if bakedEnv != nil {
+		c.Env = make(map[string]string)
+		for i, pair := range bakedEnv {
+			c.Env[fmt.Sprintf("env%d", i)] = pair
+		}
+	}
+
+	argv := []string{}
+	if args != "" {
+		argv = strings.Split(args, " ")
+	}
+	c = setRumpCmdLine(c, "program.bin", argv, addStub)
+
+	res := &types.RawImage{}
+	// add root -> sd0 mapping
+	res.RunSpec.DeviceMappings = append(res.RunSpec.DeviceMappings,
+		types.DeviceMapping{MountPoint: "/", DeviceName: "sd0"})
+
+	bootBlk := blk{
+		Source:     "dev",
+		Path:       "/dev/sd0e", // no disk label on the boot partition; so partition e is used.
+		FSType:     "blk",
+		MountPoint: "/bootpart",
+	}
+
+	c.Blk = append(c.Blk, bootBlk)
+
+	for i, mntPoint := range mntPoints {
+		deviceMapped := fmt.Sprintf("sd%ca", '1'+i)
+		blk := blk{
+			Source:     "dev",
+			Path:       "/dev/" + deviceMapped,
+			FSType:     "blk",
+			MountPoint: mntPoint,
+		}
+
+		c.Blk = append(c.Blk, blk)
+		logrus.Debugf("adding mount point to image: %s:%s", mntPoint, deviceMapped)
+		res.RunSpec.DeviceMappings = append(res.RunSpec.DeviceMappings,
+			types.DeviceMapping{MountPoint: mntPoint, DeviceName: deviceMapped})
+	}
+
+	// virtualbox network
+	c.Net = &net{
+		If:     "vioif0",
+		Type:   "inet",
+		Method: DHCP,
+	}
+
+	cmdline, err := toRumpJson(c)
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Debugf("writing rump json config: %s", cmdline)
+
+	imgFile, err := compilers.BuildBootableImage(kernel, cmdline, true, noCleanup)
+	if err != nil {
+		return nil, err
+	}
+
+	res.LocalImagePath = imgFile
+	res.StageSpec.ImageFormat = types.ImageFormat_RAW
+	res.RunSpec.DefaultInstanceMemory = 256
+	return res, nil
+}
