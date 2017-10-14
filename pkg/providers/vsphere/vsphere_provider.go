b58ad803beb2446c25f0db91f1e21067e5829fcf
sguyennet <sguyennet@unik.inkubate.io>
2017-10-14 18:40:49 +0200

    Fix error message

--- pkg/providers/vsphere/vsphere_provider.go
+++ pkg/providers/vsphere/vsphere_provider.go
@@ -51,7 +51,7 @@ func NewVsphereProvier(config config.Vsphere) (*VsphereProvider, error) {
 	p.getClient().Mkdir("unik/vsphere/volumes")
 
 	if err := p.deployInstanceListener(); err != nil {
-		return nil, errors.New("deploying virtualbox instance listener", err)
+		return nil, errors.New("deploying vsphere instance listener", err)
 	}
 
 	instanceListenerIp, err := common.GetInstanceListenerIp(instanceListenerPrefix, timeout)
